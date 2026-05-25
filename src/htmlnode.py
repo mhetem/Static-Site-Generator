02cbb0971a6044dd3fef4b34adfaddceb550b1c5
mhetem <mateushetem.mch@gmail.com>
2026-05-25 10:32:25 -0300

    Added all the conversions from text to html

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -35,3 +35,20 @@ class LeafNode(HTMLNode):
     
     def __repr__(self):
         return f"tag={self.tag}\nvalue={self.value}\nprops={self.props}"
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props = None):
+        super().__init__(tag, None, children, props)
+    
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("no tag")
+        if self.children is None:
+            raise ValueError("children missing")
+        children_string = ""
+        for string in self.children:
+            if string is not None:
+                children_string += string.to_html()
+        
+        return f'<{self.tag}{self.props_to_html()}>{children_string}</{self.tag}>'
+        
---
+++ src/test_texttohtml.py
@@ -0,0 +1,43 @@
+import unittest
+from htmlnode import *
+from textnode import *
+
+class TestTextToHtml(unittest.TestCase):
+
+    def test_textplain(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+    
+    def test_textbold(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_textitalic(self):
+        node = TextNode("This is a text node", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_textcode(self):
+        node = TextNode("This is a text node", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_textlink(self):
+        node = TextNode("This is a text node", TextType.LINK, "this is an url")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a text node")
+        self.assertEqual(html_node.props, {"href": "this is an url"})
+
+    def test_textimage(self):
+        node = TextNode("This is a text node", TextType.IMAGE, "this is a link")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props, {"src": "this is a link", "alt": "This is a text node"})
